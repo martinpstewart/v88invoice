bf596e0eb87b94a050b37f4ade6f65152ddd36de
martinpstewart <155626500+martinpstewart@users.noreply.github.com>
2024-03-26 13:20:43 +0000

    Update function.js

--- function.js
+++ function.js
@@ -7,7 +7,7 @@ window.function = function (html, fileName, format, zoom, orientation, margin, b
 	};
 
 	// DYNAMIC VALUES
-	html = html.value ?? "<b>Hello World</b>";
+	html = html.value ?? "<html><b>Hello World</b></html>";
 	fileName = fileName.value ?? "file";
 	format = format.value ?? "a4";
 	zoom = zoom.value ?? "1";
